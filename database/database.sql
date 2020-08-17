0651aed6c5d1147c50441602c0b2894951296846
Leroy Grant <lgrant146@gmail.com>
2020-08-17 01:26:46 -0500

    finished

---
+++ database/database.sql
@@ -0,0 +1,7 @@
+USE demo_db;
+
+DROP TABLE users;
+insert into users (username, email, password) values
+('bob', 'bob@email.com', 'bob'),
+('plant_grl', 'plant@email.com', 'iloveplants'),
+('bluecloud', 'blue@email.com', 'clouds');
---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,66 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
